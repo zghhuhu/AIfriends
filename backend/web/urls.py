b97ca11971d8a52fc4200f43a1e59a5868611b0a
ac one <3512097502@qq.com>
2026-02-04 15:32:43 +0800

    创建更新用户资料api

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -1,11 +1,12 @@
 
-from django.urls import path
+from django.urls import path, re_path
 from web.views.index import index
 from web.views.user.account.get_user_info import GetUserInfoView
 from web.views.user.account.login import LoginView
 from web.views.user.account.logout import LogoutView
 from web.views.user.account.refresh_token import RefreshTokenView
 from web.views.user.account.register import RegisterView
+from web.views.user.profile.update import UpdateProfileView
 
 urlpatterns = [
     path('api/user/account/login/', LoginView.as_view()),
@@ -13,6 +14,9 @@ urlpatterns = [
     path('api/user/account/register/', RegisterView.as_view()),
     path('api/user/account/refresh_token/', RefreshTokenView.as_view()),
     path('api/user/account/get_user_info/', GetUserInfoView.as_view()),
+    path('api/user/profile/update/', UpdateProfileView.as_view()),
 
     path('', index),
+
+    re_path(r'^(?!media/|static/|assets/).*$', index)
 ]
