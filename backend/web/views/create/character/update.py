8f9f802441c3b1c0abeb66c4b58e452347efdca3
ac one <3512097502@qq.com>
2026-02-06 09:56:22 +0800

    实现了创建角色

--- backend/web/views/create/character/update.py
+++ backend/web/views/create/character/update.py
@@ -40,6 +40,6 @@ class UpdateCharacterView(APIView):
                 'result': 'success',
             })
         except:
-            Response({
+           return Response({
                 'result': '系统异常，请稍后重试'
             })
