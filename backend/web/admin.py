40cda1aefc90f4acfcf9ad653dd9db39d09eda78
ac one <3512097502@qq.com>
2026-02-13 20:38:17 +0800

    实现了好友后端

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from web.models.user import UserProfile
 from web.models.character import Character
+from web.models.friend import Friend
 
 
 @admin.register(UserProfile)
@@ -11,3 +12,8 @@ class UserProfileAdmin(admin.ModelAdmin):
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)
+
+
+@admin.register(Friend)
+class FriendAdmin(admin.ModelAdmin):
+    raw_id_fields = ('me', 'character',)
